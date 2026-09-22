219527f22ad75971f6db26d282221f19f342555b
jfsteinWM <jfstein@wm.edu>
2026-09-22 17:54:16 -0400

    Add files via upload

--- date.py
+++ date.py
@@ -2,7 +2,7 @@ date = input("Enter the date in US format (MM/DD/YY): ")
 month, day, year = date.split("/")
 iso_date = "20" + year + "-" + month + "-" + day
 
-print("the date in ISO 8601 extended format is:", iso_date)
+print("The date in ISO 8601 extended format is:", iso_date)
 
 
 
