bba502cc3c014b8f12e387d0d1596bb7bb61fafd
jfsteinWM <jfstein@wm.edu>
2026-09-22 21:11:57 -0400

    Add files via upload

--- currency.py
+++ currency.py
@@ -1,13 +1,30 @@
 US = float(1.00)
-EUR = float(1.08 * US)
-GBP = float(1.21 * US)
-CNY = float(0.15 * US)
-INR = float(0.012 * US)
+EUR = float(US/1.08)
+GBP = float(US/1.21)
+CNY = float(US/0.15)
+INR = float(US/0.012)
 
-transaction_fee = US * 0.95
+transaction_fee = 0.95
 
 convert = input("Choose to convert (1. EUR, 2. GBP, 3. CNY, 4. INR): ")
 
-dollar_amount = input("Enter dollar amount to exchange: ")
+dollar_amount = float(input("Enter dollar amount to exchange: ").replace("$", ""))
 
-print("After fees you will receive", (dollar_amount * transaction_fee * convert, "in your chosen currency."))
+rates = {
+    "1": EUR,
+    "2": GBP,
+    "3": CNY,
+    "4": INR
+}
+
+currencies = {
+    "1": "EUR",
+    "2": "GBP",
+    "3": "CNY",
+    "4": "INR"
+}
+
+amount_after_fee = dollar_amount * transaction_fee
+converted_amount = amount_after_fee * rates[convert]
+
+print("After fees you will receive", converted_amount, currencies[convert], "in your chosen currency.")
